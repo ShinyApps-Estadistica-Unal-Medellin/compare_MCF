a4e60b9c2c42159c3f93a5ccc8aa86148077e1b4
Carlos M. Lopera-Gomez <cmlopera@unal.edu.co>
2025-07-25 23:07:54 -0500

    App_format

--- app.R
+++ app.R
@@ -35,33 +35,38 @@ ui <- fluidPage(
       
       hr(),
       
-      h4("Columnas Dataset 1"),
+      h4("Variables de la base de datos 1"),
       uiOutput("col_id1")%>%
-        tagAppendAttributes(`data-step` = 7,`data-intro` = "Selecciona la variable de identificación del individuo del primer conjunto de datos"),
+        tagAppendAttributes(`data-step` = 7,`data-intro` = "Selecciona la variable de identificación del equipo o sistema en la primera base de datos."),
       uiOutput("col_time1") %>%
-        tagAppendAttributes(`data-step` = 8,`data-intro` = "Selecciona la variable del tiempo del primer conjunto de datos"),
+        tagAppendAttributes(`data-step` = 8,`data-intro` = "Selecciona la variable de tiempo a falla o censura en la primera base de datos."),
       uiOutput("col_event1")  %>%
-        tagAppendAttributes(`data-step` = 9,`data-intro` = "Selecciona la variable del evento  del primer de datos"),
+        tagAppendAttributes(`data-step` = 9,`data-intro` = "Selecciona la variable indicadora de falla o censura en la primera base de datos."),
       
-      h4("Columnas Dataset 2"),
+      h4("Variables de la base de datos 2"),
       uiOutput("col_id2") %>%
-        tagAppendAttributes(`data-step` = 10,`data-intro` = "Selecciona la variable de identificación del individuo del segundo conjunto de datos"),
+        tagAppendAttributes(`data-step` = 10,`data-intro` = "Selecciona la variable de identificación del equipo o sistema en la segunda base de datos."),
       uiOutput("col_time2") %>%
-        tagAppendAttributes(`data-step` = 11,`data-intro` = "Selecciona la variable del tiempo del segundo conjunto de datos"),
+        tagAppendAttributes(`data-step` = 11,`data-intro` = "Selecciona la variable de tiempo a falla o censura en la segunda base de datos."),
       uiOutput("col_event2")  %>%
-        tagAppendAttributes(`data-step` = 12,`data-intro` = "Selecciona la variable del evento del segundo conjunto de datos")
+        tagAppendAttributes(`data-step` = 12,`data-intro` = "Selecciona la variable indicadora de falla o censura en la segunda base de datos."),
+      h4("Autores:"),
+      tags$ul(
+        tags$li(tags$a(href="mailto:datobonv@unal.edu.co", "Davinson Tobón-Viana")),
+        tags$li(tags$a(href="mailto:mcjarami@unal.edu.co", "Mario C. Jaramillo-Elorza")),
+        tags$li(tags$a(href="mailto:cmlopera@unal.edu.co", "Carlos M. Lopera-Gómez"))
+      ),
+      h4("Correspondencia:"),
+      tags$ul(
+        tags$li(tags$a(href="mailto:mcjarami@unal.edu.co", "Mario C. Jaramillo-Elorza"))
+      )
     ),
     
     mainPanel(
       tabsetPanel(
         tabPanel("Resumen", 
                  verbatimTextOutput("summary_text") %>%
-                   tagAppendAttributes(`data-step` = 13,`data-intro` = "En la pestaña resumen, podras visualizar un resumen 
-                   estadístico de los dos conjuntos de datos convertidos al formato RDU, incluyendo información como 
-                   número de recurrencias, tiempo minimo de las recurrencias, tiempo maximo de las recurrencias,etc. Y en 
-                   la pestaña Gráficas, puedes visualizar la comparación de las curvas MCF (Mean Cumulative Function) 
-                   para los dos conjuntos de datos. La primera gráfica muestra ambas curvas superpuestas, y la segunda 
-                   muestra la diferencia entre ellas.") 
+                   tagAppendAttributes(`data-step` = 13,`data-intro` = "En la pestaña resumen, podras visualizar un resumen estadístico de los dos conjuntos de datos convertidos al formato RDU, incluyendo información como número de recurrencias, tiempo minimo de las recurrencias, tiempo maximo de las recurrencias,etc. Y en la pestaña Gráficas, puedes visualizar la comparación de las curvas MCF (Mean Cumulative Function) para los dos conjuntos de datos. La primera gráfica muestra ambas curvas superpuestas, y la segunda muestra la diferencia entre ellas.") 
         ),
         
         tabPanel("Gráficas", 
@@ -69,17 +74,18 @@ ui <- fluidPage(
                      plotOutput("mcf_plot", height = "600px") 
                        
                  )
-        )
+        ) %>%
+        tagAppendAttributes(`data-step` = 14, `data-intro` = "En la pestaña Gráficas, podrás visualizar la comparación de las curvas MCF (Mean Cumulative Function) para las dos bases de datos seleccionadas o cargadas. La primera gráfica muestra ambas curvas superpuestas y la segunda muestra la diferencia entre ellas.")
         
       )
     )
-  ),
-  # Marca de agua
-
-  
-  tags$div(
-    style = "text-align: center; font-size: 13px; color: #888; margin-top: 40px; margin-bottom: 10px;",
-    "Creado por Grupo de investigación en Estadística, Universidad Nacional de Colombia - Sede Medellín"
+  # ),
+  # # Marca de agua
+  # 
+  # 
+  # tags$div(
+  #   style = "text-align: center; font-size: 13px; color: #888; margin-top: 40px; margin-bottom: 10px;",
+  #   "Creado por Grupo de investigación en Estadística, Universidad Nacional de Colombia - Sede Medellín"
   )
 )
 
@@ -128,8 +134,8 @@ server <- function(input, output, session) {
   
   output$data_ui1 <- renderUI({
     if (input$data_input == "predefined") {
-      selectInput("base_datos1", "Selecciona Dataset 1:", choices = bases_de_datos) %>%
-        tagAppendAttributes(`data-step` = 2, `data-intro` = "Selecciona el primer conjunto de datos precargado.")
+      selectInput("base_datos1", "Selecciona la base de datos 1:", choices = bases_de_datos) %>%
+        tagAppendAttributes(`data-step` = 2, `data-intro` = "Selecciona la primera base de datos precargado.")
     } else {
       tagList(
         fileInput("file1", "Sube archivo para Dataset 1:") %>%
@@ -144,7 +150,7 @@ server <- function(input, output, session) {
 
   output$data_ui2 <- renderUI({
     if (input$data_input == "predefined") {
-      selectInput("base_datos2", "Selecciona Dataset 2:", choices = bases_de_datos) %>%
+      selectInput("base_datos2", "Selecciona la base de datos 2:", choices = bases_de_datos) %>%
       tagAppendAttributes(`data-step` = 4, `data-intro` = "Selecciona el segundo conjunto de datos precargado.")
     } else {
       tagList(
@@ -175,49 +181,47 @@ server <- function(input, output, session) {
   
   output$col_id1 <- renderUI({
     req(datos1())
-    selectInput("col_id1", "Columna ID:", choices = NULL)
+    selectInput("col_id1", "Variable ID:", choices = NULL)
   })
   
   output$col_time1 <- renderUI({
     req(datos1())
-    selectInput("col_time1", "Columna tiempo:", choices = NULL)
+    selectInput("col_time1", "Variable tiempo:", choices = NULL)
   })
   
   output$col_event1 <- renderUI({
     req(datos1())
-    selectInput("col_event1", "Columna tipo evento:", choices = NULL)
+    selectInput("col_event1", "Variable tipo evento:", choices = NULL)
   })
   
   output$col_id2 <- renderUI({
     req(datos2())
-    selectInput("col_id2", "Columna ID:", choices = NULL)
+    selectInput("col_id2", "Variable ID:", choices = NULL)
   })
   
   output$col_time2 <- renderUI({
     req(datos2())
-    selectInput("col_time2", "Columna tiempo:", choices = NULL)
+    selectInput("col_time2", "Variable tiempo:", choices = NULL)
   })
   
   output$col_event2 <- renderUI({
     req(datos2())
-    selectInput("col_event2", "Columna tipo evento:", choices = NULL)
+    selectInput("col_event2", "Variable tipo evento:", choices = NULL)
   })
   
   rdu1 <- reactive({
     req(datos1(), input$col_id1, input$col_time1)
-    col_event <- if (input$col_event1 != "No aplica") input$col_event1 else NULL
     frame.to.rdu(datos1(), ID.column = input$col_id1,
                           time.column = input$col_time1,
-                          event.column = col_event,
+                          event.column = input$col_event1,
                           data.title = {input$base_datos1 %||% input$file1$name})
   })
   
   rdu2 <- reactive({
     req(datos2(), input$col_id2, input$col_time2)
-    col_event <- if (input$col_event2 != "No aplica") input$col_event2 else NULL
     frame.to.rdu(datos2(), ID.column = input$col_id2,
                           time.column = input$col_time2,
-                          event.column = col_event,
+                          event.column = input$col_event2,
                           data.title = {input$base_datos2 %||% input$file2$name})
   })
   
@@ -237,10 +241,8 @@ server <- function(input, output, session) {
   
   output$summary_text <- renderPrint({ 
     req(rdu1(), rdu2())
-    cat("Resumen Dataset 1:\n")
-    print(summary(rdu1()))
-    cat("\nResumen Dataset 2:\n")
-    print(summary(rdu2())) 
+    summary(rdu1())
+    summary(rdu2())
   })
 
 
